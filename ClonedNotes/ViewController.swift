f7f79aed93b630d3f74145799ffe922c5e0862a5
Michele Galvagno <michicello@gmail.com>
2019-04-22 19:06:06 +0200

    Initial Commit

---
+++ ClonedNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClonedNotes
+//
+//  Created by Michele Galvagno on 22/04/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
